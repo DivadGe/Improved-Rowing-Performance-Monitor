c9e360822b6360ba653e3c4ae16ecd74b6fb9e0f
David Gerits <94554914+DivadGe@users.noreply.github.com>
2023-06-07 07:36:59 -0700

    Update launcher.sh

--- DataLogger/launcher.sh
+++ DataLogger/launcher.sh
@@ -1,10 +1,3 @@
-#!/bin/sh
-# launcher.sh
-# navigate to home directory, then to this directory, then execute python script, then back home
-
-cd /
-cd home/orangepi/Documents/DataLogging
-sleep 1m
 sudo python3 settime.py
 sudo python3 LoggerMultiProcess.py > log$(date "+%H:%M").txt
 
